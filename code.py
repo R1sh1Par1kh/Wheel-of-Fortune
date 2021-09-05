2d312b92415f9c253e5761ea62e598e672271cbf
Rishi Parikh <89932552+R1sh1Par1kh@users.noreply.github.com>
2021-09-05 11:59:34 -0400

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,9 @@
+# main.py
+
+#change answer value to prefrence but meep it as a string. 
 answer = "pepperoni"
 letters = set()
+guessNum = 15
 
 def listToString(list):
   word = ""
@@ -7,11 +11,10 @@ def listToString(list):
     word += item
   return word
 
-print("Welcome to the Wheel of Fortune! You have 15 guesses to figure out the correct word. Good luck!")
+print("Welcome to the Wheel of Fortune! You have " + str(guessNum) + " guesses to figure out the correct word. Good luck!")
 
 currentState = []
 currentStateStr = ""
-
 for letter in answer:
   currentState.append("_ ")
   letters.add(letter)
@@ -20,19 +23,21 @@ for letter in answer:
 currentStateStr = listToString(currentState)
 print(currentStateStr)
 
-guessNum = 15
-
-for i in range(guessNum, 0, -1):
-  guess = input("Guess a letter! You have " + str(i) + " guesses remaining. ")
+while True:
+  if guessNum == 0:
+    print("Your'e out of guesses, meaning you lost the game!")
+    break
   
-  
+  guess = input("Guess a letter! You have " + str(guessNum) + " guesses remaining. ")
+  guessNum -= 1
+    
   if guess in letters:
     for i in range(len(answer)):
       if guess == answer[i]:
         currentState[i] = guess
     currentStateStr = listToString(currentState)
   print(currentStateStr)
-  
+    
   if currentStateStr == answer:
     print("Congratulations, you win")
     break
